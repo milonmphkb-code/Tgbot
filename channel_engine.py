a9943e8cc746a475f7866cb39596bfc8408273fb
milonmphkb-code <milonmphkb@gmail.com>
2026-08-17 17:46:42 +0600

    Delete channel_engine.py

--- channel_engine.py
+++
@@ -1,8 +0,0 @@
-from app.logging_setup import logger
-
-async def post_to_channel(context, channel_id: str, text: str):
-    try:
-        await context.bot.send_message(chat_id=channel_id, text=text)
-        logger.info("Successfully posted to channel.")
-    except Exception as e:
-        logger.error(f"Failed to post to channel: {e}")
