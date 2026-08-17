a6176b285e21c6bcb82e6ccf96cbacf43f178a52
milonmphkb-code <milonmphkb@gmail.com>
2026-08-17 17:46:20 +0600

    Delete backup.py

--- backup.py
+++
@@ -1,6 +0,0 @@
-from app.logging_setup import logger
-
-def run_backup():
-    logger.info("Backup process started...")
-    # এখানে ব্যাকআপ নেওয়ার লজিক থাকবে
-    logger.info("Backup completed successfully.")
