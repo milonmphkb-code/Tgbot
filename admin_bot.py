1c11aabd3180cc08fffa00d35957a4f7b71e6046
milonmphkb-code <milonmphkb@gmail.com>
2026-08-17 17:47:03 +0600

    Delete admin_bot.py

--- admin_bot.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from app.logging_setup import logger
-
-async def admin_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.info("Admin command triggered")
-    await update.message.reply_text("হ্যালো অ্যাডমিন! আমি প্রস্তুত।")
